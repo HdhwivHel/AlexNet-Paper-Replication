c3e6239af3b63cf8cbec71855b286812a4820ea1
yug pavasiya <yugpavasiya@outlook.com>
2026-03-11 18:06:44 +0530

    model architecture finished

--- models/alexnet.py
+++ models/alexnet.py
@@ -4,8 +4,40 @@ import torch
 
 class AlexNet(nn.Module):
     def __init__(self):
-        super.__init__()
-        self.conv1 = nn.Conv2d()
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(
+                in_channels=96, out_channels=256, kernel_size=5, padding=2, stride=1
+            ),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=384, out_channels=256, stride=1, kernel_size=3, padding=1
+            ),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # Flatten the output of the convolutional layers to feed into the fully connected layers
+            nn.Flatten(),
+            # ---Fully Connected Layers---
+            nn.Linear(in_features=256 * 6 * 6, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(
+                in_features=4096, out_features=100
+            ),  # training only for 100 classes
+        )
 
-        def forward(self, x):
-            return x
+    def forward(self, x):
+        return self.network(x)

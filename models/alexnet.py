f443dbfde632b8a874e36e6d0cf7dffb309b786c
yug pavasiya <yugpavasiya@outlook.com>
2026-03-11 12:51:16 +0530

    initial commit

---
+++ models/alexnet.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+import torch
+
+
+class AlexNet(nn.Module):
+    def __init__(self):
+        super.__init__()
+        self.conv1 = nn.Conv2d()
+
+        def forward(self, x):
+            return x

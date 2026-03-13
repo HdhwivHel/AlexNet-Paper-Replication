265494983a98c261ce5a25f745cc642b58cc33ac
yug pavasiya <yugpavasiya@outlook.com>
2026-03-13 15:35:53 +0530

    added train and test transforms

---
+++ dataset/data_pipeline.py
@@ -0,0 +1,20 @@
+import torchvision.transforms.v2 as transforms
+import torch
+
+
+def get_dataset():
+    train_transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.RandomCrop(227),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ]
+    )
+    test_transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(227),
+            transforms.ToTensor(),
+        ]
+    )
